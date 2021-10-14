1c74443f6c7a73cd29a8acb93e214262c25c4e85
Piresssss <faelpiressilva@hotmail.com>
2021-10-14 19:45:38 -0300

    category by id, exception handler

--- backend/src/main/java/com/rpires/crudwsrest/services/ClientService.java
+++ backend/src/main/java/com/rpires/crudwsrest/services/ClientService.java
@@ -1,6 +1,7 @@
 package com.rpires.crudwsrest.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.rpires.crudwsrest.dto.ClientDTO;
 import com.rpires.crudwsrest.entities.Client;
 import com.rpires.crudwsrest.repositories.ClientRepository;
+import com.rpires.crudwsrest.services.exceptions.EntityNotFoundException;
 
 /* Camada de Serviço */
 
@@ -24,6 +26,13 @@ public class ClientService {
 	public List<ClientDTO> findAll() {
 		List<Client> list = repository.findAll();
 		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
+	}
+
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+		return new ClientDTO(entity);
 	}
 	
 }
