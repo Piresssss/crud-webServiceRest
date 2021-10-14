646ba32b259ab382363ba74301d7b55bb9069af9
Piresssss <faelpiressilva@hotmail.com>
2021-10-14 18:28:27 -0300

    backend project created

---
+++ backend/src/main/java/com/rpires/crudwsrest/CrudWsrestApplication.java
@@ -0,0 +1,13 @@
+package com.rpires.crudwsrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudWsrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudWsrestApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/rpires/crudwsrest/CrudWsrestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rpires.crudwsrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudWsrestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
