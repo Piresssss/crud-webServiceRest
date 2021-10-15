7a35fc6a7014baa325258c020e70c18732d47f90
Piresssss <faelpiressilva@hotmail.com>
2021-10-14 21:00:52 -0300

    post, put and delete created

--- backend/src/main/java/com/rpires/crudwsrest/resources/ClientResource.java
+++ backend/src/main/java/com/rpires/crudwsrest/resources/ClientResource.java
@@ -1,13 +1,19 @@
 package com.rpires.crudwsrest.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.rpires.crudwsrest.dto.ClientDTO;
 import com.rpires.crudwsrest.services.ClientService;
@@ -19,13 +25,15 @@ public class ClientResource {
 	@Autowired
 	private ClientService service;
 	
+	/* método para buscar todas as categorias */
+	
 	@GetMapping
 	public ResponseEntity<List<ClientDTO>> findAll() {
 		List<ClientDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 	
-	/*método para buscar uma categoria por id*/
+	/* método para buscar uma categoria por id */
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
@@ -33,4 +41,28 @@ public class ClientResource {
 		return ResponseEntity.ok().body(dto);
 	}
 	
+	/* método para inserir no banco uma nova categoria */
+	
+	@PostMapping
+	public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO dto) {
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+	}
+	
+	/* método para atualizar */
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<ClientDTO> update(@PathVariable Long id, @RequestBody ClientDTO dto) {
+		dto = service.update(id, dto);
+		return ResponseEntity.ok().body(dto);
+	}
+	
+	/* método para deletar */
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 }
--- backend/src/main/java/com/rpires/crudwsrest/services/ClientService.java
+++ backend/src/main/java/com/rpires/crudwsrest/services/ClientService.java
@@ -4,14 +4,19 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.rpires.crudwsrest.dto.ClientDTO;
 import com.rpires.crudwsrest.entities.Client;
 import com.rpires.crudwsrest.repositories.ClientRepository;
-import com.rpires.crudwsrest.services.exceptions.EntityNotFoundException;
+import com.rpires.crudwsrest.services.exceptions.DatabaseException;
+import com.rpires.crudwsrest.services.exceptions.ResourceNotFoundException;
 
 /* Camada de Serviço */
 
@@ -33,6 +38,39 @@ public class ClientService {
 		Optional<Client> obj = repository.findById(id);
 		Client entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
 		return new ClientDTO(entity);
+	}
+
+	@Transactional
+	public ClientDTO insert(ClientDTO dto) {
+		Client entity = new Client();
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+		return new ClientDTO(entity);
+	}
+	
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO dto) {
+		try {
+		Client entity = repository.getOne(id);
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+		return new ClientDTO(entity);
+		}
+		catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("ID not found" + id);
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+		repository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("ID not found" + id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
 	}
 	
 }
